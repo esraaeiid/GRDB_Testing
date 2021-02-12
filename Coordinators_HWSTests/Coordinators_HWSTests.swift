434ff73ad7466b9e5f131fadf4a74e80356b3284
Esraa Eid <esraa@ryets.com>
2021-02-12 07:10:56 -0800

    Initial Commit

---
+++ Coordinators_HWSTests/Coordinators_HWSTests.swift
@@ -0,0 +1,33 @@
+//
+//  Coordinators_HWSTests.swift
+//  Coordinators_HWSTests
+//
+//  Created by Esraa Eid on 12/02/2021.
+//
+
+import XCTest
+@testable import Coordinators_HWS
+
+class Coordinators_HWSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
