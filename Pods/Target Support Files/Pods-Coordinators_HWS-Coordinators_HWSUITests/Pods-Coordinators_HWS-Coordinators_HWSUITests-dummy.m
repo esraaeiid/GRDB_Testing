9ce96766dd87b93b33beb1055e205fee3a4595b5
EsraaEid <esraa@ryets.com>
2021-08-09 17:35:05 +0200

    GRDB testing

---
+++ Pods/Target Support Files/Pods-Coordinators_HWS-Coordinators_HWSUITests/Pods-Coordinators_HWS-Coordinators_HWSUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Coordinators_HWS_Coordinators_HWSUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Coordinators_HWS_Coordinators_HWSUITests
+@end
---
+++ Pods/Target Support Files/Pods-Coordinators_HWSTests/Pods-Coordinators_HWSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Coordinators_HWSTests : NSObject
+@end
+@implementation PodsDummy_Pods_Coordinators_HWSTests
+@end
